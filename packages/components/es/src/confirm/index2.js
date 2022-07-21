1db585d15dc7f20e1ba472426672eeb6ff68769f
RtyXmd <rongtianyang@inin88.com>
2022-07-21 11:03:29 +0800

    'fix11'

---
+++ packages/components/es/src/confirm/index2.js
@@ -0,0 +1,15 @@
+import { openBlock, createElementBlock, pushScopeId, popScopeId, createElementVNode } from "vue";
+import "./index.vue_vue_type_style_index_0_scoped_true_lang.js";
+import _export_sfc from "../../_virtual/plugin-vue_export-helper.js";
+const _sfc_main = {};
+const _withScopeId = (n) => (pushScopeId("data-v-79ff8842"), n = n(), popScopeId(), n);
+const _hoisted_1 = { class: "main" };
+const _hoisted_2 = /* @__PURE__ */ _withScopeId(() => /* @__PURE__ */ createElementVNode("div", { class: "test" }, "confirm", -1));
+const _hoisted_3 = [
+  _hoisted_2
+];
+function _sfc_render(_ctx, _cache) {
+  return openBlock(), createElementBlock("div", _hoisted_1, _hoisted_3);
+}
+var Confirm = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render], ["__scopeId", "data-v-79ff8842"]]);
+export { Confirm as default };
