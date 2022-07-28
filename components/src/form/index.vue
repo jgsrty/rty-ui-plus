0c87e5c272df3abada3a3d24aaa72afe8df03804
RtyXmd <rongtianyang@inin88.com>
2022-07-28 11:23:47 +0800

    fix form

--- components/src/form/index.vue
+++ components/src/form/index.vue
@@ -2,99 +2,43 @@
   <form @submit.prevent><slot /></form>
 </template>
 
-<script>
-const formEmits = {
-  validate: (prop, isValid, message) => {
-    console.log(prop)
-    console.log(isValid)
-    console.log(message)
-    return true;
-  },
-};
-</script>
-
 <script setup>
-import { provide, reactive, toRefs, watch } from "vue";
+import { provide, reactive, toRefs } from "vue";
+import AsyncValidator from "async-validator";
 
 const props = defineProps({
   model: {
     type: Object,
     required: true,
   },
-  labelPosition: {
-    type: String,
-    values: ["left", "right", "center"],
-    default: "right",
-  },
   rules: {
     type: Object,
   },
 });
-const emits = defineEmits(formEmits);
 
 const fields = [];
 
 const addField = (field) => {
   fields.push(field);
 };
-const removeField = (field) => {
-  if (field.prop) {
-    fields.splice(fields.indexOf(field), 1);
-  }
+const validate = (callback) => {
+  let validator = new AsyncValidator(props.rules);
+  let validSuccess = true;
+  validator.validate(props.model, (errors) => {
+    fields.forEach((formItem) => {
+      let error = errors?.find((error) => error.field === formItem.prop);
+      if (error) validSuccess = false;
+      formItem.validateMessage = (error && error.message) || "";
+    });
+  });
+  callback(validSuccess);
 };
-const validate = async (callback) => {
-  validateField(undefined, callback);
-};
-const validateField = async (modelProps, callback) => {
-  try {
-    const result = await doValidateField(modelProps);
-    if (result) {
-      callback?.(result);
-    }
-    return result;
-  } catch (e) {
-    const invalidFields = e;
-
-    // if (props.scrollToError) {
-    //   scrollToField(Object.keys(invalidFields)[0])
-    // }
-    callback?.(false, invalidFields);
-    return typeof callback !== "function" && Promise.reject(invalidFields);
-  }
-};
-const doValidateField = async (props) => {
-  let validationErrors = {};
-  console.log(props);
-  // if (!props) return true;
-  for (const field of fields) {
-    try {
-      await field.validate("");
-    } catch (err) {
-      validationErrors = {
-        ...validationErrors,
-        ...fields,
-      };
-    }
-  }
-  if (Object.keys(validationErrors).length === 0) return true;
-  return Promise.reject(validationErrors);
-};
-
-watch(
-  () => props.rules,
-  (val) => {
-    console.log(val);
-  },
-  { deep: true }
-);
 
 provide(
   "rty-form",
   reactive({
     ...toRefs(props),
-    emits,
     addField,
-    removeField,
   })
 );
 
