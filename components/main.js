9ef653cc2caba27d1b0561d030155bea98293b2a
RtyXmd <rongtianyang@inin88.com>
2022-07-29 17:30:26 +0800

    date init

--- components/main.js
+++ components/main.js
@@ -1,8 +1,8 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-// import rtyUiPlus from "./index";
+import rtyUiPlus from "./index";
 
 const app = createApp(App);
 
-// app.use(rtyUiPlus).mount("#app");
-app.mount("#app");
+app.use(rtyUiPlus).mount("#app");
+// app.mount("#app");
---
+++ components/src/date/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div></div>
+</template>
+
+<script setup>
+import { generateDateData } from "./utils";
+const test = generateDateData([2022, 7]);
+console.log(test);
+</script>
+
+<style lang="scss" scoped>
+@import "./index.scss";
+</style>
